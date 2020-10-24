22c72ff8fd08e5019888fe9e662aec1a1d43ca13
andy19910102 <andy19910102@gmail.com>
2020-10-24 14:19:21 +0800

    Project init

---
+++ components/NavigationBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav class="navigation-bar">
+      <nuxt-link class="brand" to="/">Travel Blog</nuxt-link>
+      <button @click="toggleActive" class="menu-btn">
+        <span class="menu-btn-bar"></span>
+      </button>
+      <div class="nav-list">
+        <nuxt-link class="brand" to="/">Home</nuxt-link>
+        <nuxt-link class="brand" to="/about">About</nuxt-link>
+        <a>Sign In</a>
+        <a>Register</a>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavigationBar",
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    toggleActive() {},
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- Header -->
+    <header class="jumbotron">
+      <div class="container">
+        <h1>Welcome to Travel Blog</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+          placeat accusantium? Suscipit in at ducimus tenetur eveniet! Modi
+          saepe, vel voluptates totam ullam tempore esse architecto, ab et error
+          facilis!
+        </p>
+        <nuxt-link class="btn btn-primary" to="/about">More Info</nuxt-link>
+      </div>
+    </header>
+    <!-- Header end -->
+    <!-- ArticleSection -->
+    <section class="py-5">
+      <div class="container">
+        <div class="section-title-group">
+          <h2 class="section-title text-center">Article List</h2>
+          <div class="section-title-bar bg-warning"></div>
+        </div>
+        <!-- ArticleList -->
+
+        <!-- ArticleList end -->
+      </div>
+    </section>
+    <!-- ArticleSection -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style>
+</style>
