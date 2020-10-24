e293462dee5ae1afad4d62c7bf10c5039e03d574
andy19910102 <andy19910102@gmail.com>
2020-10-24 14:36:17 +0800

    Add article pages && Footer

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,16 @@
       </div>
     </section>
     <!-- ArticleSection -->
+    <!-- Footer -->
+    <footer class="py-3">
+      <div class="container text-center">
+        <h5>
+          <nuxt-link to="/">Nuxt Travel Blog</nuxt-link>
+        </h5>
+        <p class="mb-0">2020 &copy; All Rights Reserved</p>
+      </div>
+    </footer>
+    <!-- Footer end -->
   </div>
 </template>
 
